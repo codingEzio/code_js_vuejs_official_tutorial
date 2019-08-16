bf5376a83798f38c442a2ab7ebd847c3163ab115
Ezio <assassinste@gmail.com>
2019-08-16 18:37:26 +0800

    A brand-new todo-list app (ah)

--- Unit05_Tooling/Part01_SingleFileComponents/todo-list/src/App.vue
+++ Unit05_Tooling/Part01_SingleFileComponents/todo-list/src/App.vue
@@ -1,28 +1,39 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+	<div id="app">
+		<h1>My fancy Todo App 🤗</h1>
+		<TodoList/>
+	</div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+    import TodoList from "./components/TodoList.vue";
 
-export default {
-  name: 'app',
-  components: {
-    HelloWorld
-  }
-}
+    export default {
+        name: 'app',
+        components: {
+            TodoList,
+        }
+    };
 </script>
 
 <style lang="scss">
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+	@import "./styles/variables.scss";
+	
+	*, *::before, *::after {
+		box-sizing: border-box;
+	}
+	
+	#app {
+		max-width: 400px;
+		margin: 0 auto;
+		line-height: 1.4;
+		font-family: 'Rubik', 'Roboto', sans-serif;
+		-webkit-font-smoothing: antialiased;
+		-moz-osx-font-smoothing: grayscale;
+		color: $vue-blue;
+	}
+	
+	h1 {
+		text-align: center;
+	}
 </style>
--- Unit05_Tooling/Part01_SingleFileComponents/todo-list/src/main.js
+++ Unit05_Tooling/Part01_SingleFileComponents/todo-list/src/main.js
@@ -1,8 +1,15 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from 'vue/dist/vue.js';
+import App from './App.vue';
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
+
+// Change `from vue` to `from 'vue/dist/vue.js'`
+//  this solution was suggested by the author of Vue.js
+//  WHY     you wanna write the init code like `new Vue({..})`
+//  LINK    https://github.com/vuejs-templates/webpack/issues/215#issuecomment-238095102
 
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+    el: '#app',
+    template: '<App/>',
+    components: {App}
+});
